77e538efeb2c9d300455ca16c9b0c1e2af6cf0e2
jillo-abdullahi <jillo.abdulla@gmail.com>
2023-09-12 19:01:08 +0300

    refactor - change font family

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,15 @@
-import { useState } from 'react';
 import NextApp, { AppProps, AppContext } from 'next/app';
-import { getCookie, setCookie } from 'cookies-next';
+import { Merriweather, PT_Sans } from 'next/font/google';
 import Head from 'next/head';
-import { MantineProvider, ColorScheme, ColorSchemeProvider } from '@mantine/core';
-import { Notifications } from '@mantine/notifications';
+import { MantineProvider, ColorScheme } from '@mantine/core';
+
+const barlowCondensed = PT_Sans({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
 
 export default function App(props: AppProps & { colorScheme: ColorScheme }) {
   const { Component, pageProps } = props;
-  const [colorScheme, setColorScheme] = useState<ColorScheme>(props.colorScheme);
-
-  const toggleColorScheme = (value?: ColorScheme) => {
-    const nextColorScheme = value || (colorScheme === 'dark' ? 'light' : 'dark');
-    setColorScheme(nextColorScheme);
-    setCookie('mantine-color-scheme', nextColorScheme, { maxAge: 60 * 60 * 24 * 30 });
-  };
 
   return (
     <>
@@ -23,12 +19,13 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
         <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
 
-      <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
-        <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
-          <Component {...pageProps} />
-          <Notifications />
-        </MantineProvider>
-      </ColorSchemeProvider>
+      <MantineProvider
+        theme={{ colorScheme: 'dark', fontFamily: barlowCondensed.style.fontFamily }}
+        withGlobalStyles
+        withNormalizeCSS
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
     </>
   );
 }
@@ -37,6 +34,5 @@ App.getInitialProps = async (appContext: AppContext) => {
   const appProps = await NextApp.getInitialProps(appContext);
   return {
     ...appProps,
-    colorScheme: getCookie('mantine-color-scheme', appContext.ctx) || 'dark',
   };
 };
